429f0e184778ad6340eb1b9656a4d457d12d7bdd
kaisbad <158204041+kaisbad@users.noreply.github.com>
2024-11-18 12:31:23 -0500

    a

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,12 @@
 //https://www.demonslayer-api.com/
+async function getData() {
+  const response = await fetch(
+    "https://www.demonslayer-api.com/api/v1/characters"
+  );
+  const data = await response.json();
+  console.log(data);
+}
+//turned into json
+/* const data = await response.json();
+console.log(response); */
+getData();
