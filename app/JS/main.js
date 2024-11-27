882925963af368922c0fb5d6f5495bf0875b71a7
kaisbad7 <158204041+kaisbad@users.noreply.github.com>
2024-11-27 00:15:11 -0500

    a

--- app/JS/main.js
+++ app/JS/main.js
@@ -6,37 +6,50 @@ async function getData() {
     //fetch returns a promise
     const response = await fetch(
       `https://api.allorigins.win/get?url=${encodeURIComponent(
-        "https://www.demonslayer-api.com/api/v1/characters"
+        "https://www.demonslayer-api.com/api/v1/characters?limit=100"
       )}`
     );
     const data = await response.json();
-    document.querySelector("h1").textContent = data.name;
-    console.log(data);
-    insertCard(data);
-  } catch {}
+    const parseddata = JSON.parse(data.contents);
+    const characters = parseddata.content;
+    console.log("Characters:", characters);
+    if (Array.isArray(characters)) {
+      characters.forEach((character) => {
+        console.log(character.name);
+      });
+      insertCard(characters);
+    } else {
+      console.error("not array", data);
+    }
+  } catch (error) {
+    console.log(error);
+    alert("error");
+  }
 }
 
 getData();
 
-function clearCards() {
+/*function clearCards() {
   DOMSelectors.container.innerHTML = "";
-}
+}*/
 
-function insertCard(data) {
-  clearCards();
+function insertCard(characters) {
+  //clearCards();
   let cardHtml = "";
-  data.forEach((character) => {
-    cardHtml += `<div class="card"><h2 class="header">${character.name}</h2>
+  characters.forEach((character) => {
+    cardHtml += `<div class="card flex flex-col items-center justify-evenly w-[17%] min-h-[25rem] border-2 border-black mb-[3%] shadow-[9px_8px_0_black] rounded-2xl">
+        <h2 class="header">${character.name}</h2>
         <ul><li>${character.gender}</li>
         <li>${character.race}</li>
         <li>${character.description}</li></ul>
-        <img src="${character.img}" alt="person" class="card-img"></div>
+        <img class="card-img h-[75%] w-auto max-w-[75%] object-cover" src="${character.img}" alt="person" class="card-img">
+        <ul><li>${character.quote}</li></ul>
+        <button class="btn text-[3rem] justify-evenly m-auto">Learn More</button>
+      </div>
         `;
   });
   DOMSelectors.container.insertAdjacentHTML("afterbegin", cardHtml);
 }
-
-insertCard();
 
 //separate api calls for each button press
 //more than one api call
